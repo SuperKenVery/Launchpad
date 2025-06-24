f32267d706a53cdc86c2c975118e3b0e39796040
ken <1610057945@qq.com>
2025-06-24 18:37:34 +0800

    Initial Commit

---
+++ launchpad/launchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  launchpadApp.swift
+//  launchpad
+//
+//  Created by ken on 2025/6/24.
+//
+
+import SwiftUI
+
+@main
+struct launchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
